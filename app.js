3768f0bff6e4ccd441eb99965d2764cafdc1d54d
Jennifer Mikac <jennymikac@gmail.com>
2020-07-21 16:21:40 -0400

    Create post object

--- app.js
+++ app.js
@@ -42,7 +42,10 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postContent
+  };
 });
 
 
